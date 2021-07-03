ef40dbf96d54da50bee0fc6e1c6e0d3481f497e6
Dennis_Arango <dennis.arango.mt@correo.usa.edu.co>
2021-07-02 21:38:57 -0500

    .

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,11 @@
-//Buscamos el elemento animado en el DOM.
-window.onload = function() {
+window.onload = function() { // We look for the animated element in the DOM.
    elFondo=document.getElementById("backAirplane");
 }
-//parar la animación: propiedad animation-play-state: paused;
-function parar() {
+function parar() { // stop animation: animation-play-state property: paused;
         elFondo.style.animationPlayState="paused"; //W3C
         elFondo.style.WebkitAnimationPlayState="paused";  //Safari y Chrome
-        }
-//reiniciar la animación: propiedad animation-play-state: running;
-function seguir() {
+}
+function seguir() { // restart animation: animation-play-state property: running;
         elFondo.style.animationPlayState="running";
         elFondo.style.WebkitAnimationPlayState="running";
 }
